9bddd8e2b853439e88c6c54f11406e1156caf4c4
riadhlaabidi <riadh.by.laabidi@gmail.com>
2024-10-10 21:35:31 +0100

    Updates

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+set -e
+
+sudo dnf install python3-libdnf5
 sudo dnf install ansible
-ansible-playbook main.yml -i inventory --ask-become-pass --connection=local
+ansible-playbook main.yml -i inventory --ask-become-pass --connection=local -v
