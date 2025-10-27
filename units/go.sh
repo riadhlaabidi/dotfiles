8706a96768c4b1c1658e2ecf5f786858d3a390e0
riadhlaabidi <riadh.by.laabidi@gmail.com>
2025-10-27 17:05:05 +0100

    Updates

--- units/go.sh
+++ units/go.sh
@@ -2,36 +2,51 @@
 
 . util.sh
 
-GO_VERSION="1.24.2"
-GO_CHECKSUM="68097bd680839cbc9d464a0edce4f7c333975e27a90246890e9f1078c7e702ad"
-GO_ARCHIVE="go$GO_VERSION.linux-amd64.tar.gz"
+GO_DOWNLOAD_URL="https://go.dev/dl/?mode=json"
+GO_ARCH="amd64"
+GO_OS="linux"
+GO_KIND="archive"
 
-pushd /usr/local >/dev/null
-if [[ -d go ]]; then
-    sudo rm -rf go/
-    echo "Remove old Go installation...[$(get_status $?)]"
+GO_STABLE=$(curl -s $GO_DOWNLOAD_URL)
+TARGET_FILTER='.[0].files.[] | select(.os == $os and .arch == $arch and .kind == $kind)'
+GO_TARGET=$(echo $GO_STABLE | jq --arg os "$GO_OS" --arg arch "$GO_ARCH" --arg kind "$GO_KIND" "$TARGET_FILTER")
+GO_ARCHIVE=$(echo $GO_TARGET | jq -r '.filename')
+GO_CHECKSUM=$(echo $GO_TARGET | jq -r '.sha256')
+
+if [[ -z $GO_ARCHIVE ]]; then
+    echo "Failed to fetch latest Go archive from $GO_DOWNLOAD_URL"
+    echo "Os: $GO_OS, Arch: $GO_ARCH, Kind: $GO_KIND"
+    exit 1
 fi
-popd >/dev/null
 
 pushd /tmp >/dev/null
 if [[ -e $GO_ARCHIVE ]]; then
     rm -rf $GO_ARCHIVE
 fi
-wget "https://go.dev/dl/$GO_ARCHIVE" >/dev/null
-echo "Download Go archive...[$(get_status $?)]"
+
+wget -q --show-progress "https://go.dev/dl/$GO_ARCHIVE" 
+echo "Download latest Go archive...[$(get_status $?)]"
 
 if [[ -e $GO_ARCHIVE ]]; then
+
     echo "$GO_CHECKSUM $GO_ARCHIVE" > go_sha.txt
 
     sha256sum --check --status go_sha.txt
     sha_ec=$?
-    echo "Verify checksum for Go archive...[$(get_status $sha_ec)]"
+    echo "Verify checksum for the downloaded Go archive...[$(get_status $sha_ec)]"
 
     if [[ sha_ec -eq 0 ]]; then
+        pushd /usr/local >/dev/null
+        if [[ -d go ]]; then
+            sudo rm -rf go/
+            echo "Remove old Go installation...[$(get_status $?)]"
+        fi
+        popd >/dev/null
         sudo tar -xf $GO_ARCHIVE -C /usr/local >/dev/null
-        echo "Unarchive go...[$(get_status $?)]"
+        echo "Unarchive Go...[$(get_status $?)]"
     fi
 
     rm -rf go_sha.txt
+    rm -rf $GO_ARCHIVE
 fi
 popd >/dev/null
